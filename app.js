a62b564c65fbc0388a0918c7cb662f99bc83cf14
adammpolak <adam.polak@citrusbyte.com>
2016-09-12 14:27:34 -0700

    Setting up html/css/js files and basic js structure

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  //script here
+  console.log("frank");
+});
+
+
+//Status of the coordinates of colored tokens
+
+//Action of dropping a token into a column
+
+//Updating the UI of the board
