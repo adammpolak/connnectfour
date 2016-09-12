4e9c8920cd49ac5d989ecff653b2f958cdb1e899
adammpolak <adam.polak@citrusbyte.com>
2016-09-12 16:25:16 -0700

    Finish setting up UI of board with space coordinates

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
   //script here
   console.log("frank");
+  var board = $('#board')
+
+  for (var x = 0; x < 7; x++) {
+    for (var y = 0; y < 6; y++) {
+      var space = $('<div>');
+      space.attr('y', y);
+      space.attr('class', 'space');
+      space.attr('x', x);
+      board.append(space);
+    };
+  };
 });
 
 
@@ -9,3 +20,19 @@ $(document).ready(function() {
 //Action of dropping a token into a column
 
 //Updating the UI of the board
+
+//check if win
+
+//setting up board
+
+var board = $('#board')
+
+for (var x = 0; x <= 7; x++) {
+  for (var y = 0; y <= 6; y++) {
+    var space = $('<div>');
+    space.attr('y', y);
+    space.attr('class', 'space');
+  };
+  space.attr('x', x);
+  board.append(space);
+};
