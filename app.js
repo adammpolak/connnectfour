c399a266ad9a98b89d75a887becbcfa6493b05b2
adammpolak <adam.polak@citrusbyte.com>
2016-09-14 18:07:35 -0700

    Refactor to make it object oriented

--- app.js
+++ app.js
@@ -1,163 +1,11 @@
 $(document).ready(function() {
   //script here
-  console.log("frank");
 
-
+// this object stores game data
 var game =  {
-
-}
-
-var movesMade = 0;
-
-//amount of moves
-
-//stores whose turn it is currently
-var currentColor = "red"
-
-//function to change current colors turn
-var changeColor = function () {
-  if (currentColor === "red") {
-    currentColor = "black";
-    $('#red').addClass('currentturn')
-    $('#black').removeClass('currentturn')
-  } else {
-    currentColor = "red"
-    $('#red').removeClass('currentturn')
-    $('#black').addClass('currentturn')
-  }
-  return currentColor;
-}
-
-//stores what colors are stored in what coordinates
-var dataStore = {
-  0: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
-  1: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
-  2: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
-  3: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
-  4: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
-  5: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
-  6: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""}
-}
-
-//stores what y position is available
-var yPositionAvailable = {
-  0: 0,
-  1: 0,
-  2: 0,
-  3: 0,
-  4: 0,
-  5: 0,
-  6: 0
-}
-
-//highlights the winning tokens
-var highlightTokens = {
-  left: function(xaxis, yaxis, amount) {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (amount>0) {
-      currentx--;
-      $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
-      amount--;
-    };
-  },
-  right: function(xaxis, yaxis, amount) {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (amount>0) {
-      currentx++;
-      $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
-      amount--;
-    };
-  },
-  down: function(xaxis, yaxis, amount) {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (amount>0) {
-      currenty--;
-      $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
-      amount--;
-    };
-  },
-  downleft: function(xaxis, yaxis, amount) {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (amount>0) {
-      currenty--;
-      currentx--;
-      $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
-      amount--;
-    };
-  },
-  upleft: function(xaxis, yaxis, amount) {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (amount>0) {
-      currenty++;
-      currentx--;
-      $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
-      amount--;
-    };
-  },
-  downright: function(xaxis, yaxis, amount) {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (amount>0) {
-      currenty--;
-      currentx++;
-      $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
-      amount--;
-    };
-  },
-  upright: function(xaxis, yaxis, amount) {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (amount>0) {
-      currenty++;
-      currentx++;
-      $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
-      amount--;
-    };
-  }
-}
-
-//if someone wins this changes the appeareance
-var winner = function () {
-  $('#center').html(currentColor.toUpperCase() + " WINS!");
-  $('#red').hide();
-  $('#black').hide();
-  $('#center').css('margin', 'auto')
-  $('#center').css('margin-bottom', '46px')
-  $('#buttonrow').hide();
-}
-
-//if there is a stalemate this changes the appearance
-var staleMate = function() {
-$('#center').html("STALE MATE!");
-$('#red').hide();
-$('#black').hide();
-$('#center').css('margin', 'auto')
-$('#center').css('margin-bottom', '46px')
-$('#buttonrow').hide();
-};
-
-var checkStaleMate = function() {
-  if (movesMade === 42) {
-    staleMate();
-  }
-};
-
-//to reset the board
-var reset = function () {
-  $('#center').html("");
-  $('#red').show();
-  $('#black').show();
-  $('#center').css('margin', 'inherit')
-  $('#buttonrow').show();
-  buttonrow.html("");
-  board.html("");
-  setup();
-  dataStore = {
+  movesMade: 0,
+  currentColor: "black",
+  dataStore: {
     0: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
     1: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
     2: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
@@ -165,8 +13,8 @@ var reset = function () {
     4: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
     5: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
     6: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""}
-  };
-  yPositionAvailable = {
+  },
+  yPositionAvailable: {
     0: 0,
     1: 0,
     2: 0,
@@ -174,145 +22,143 @@ var reset = function () {
     4: 0,
     5: 0,
     6: 0
-  };
-  movesMade = 0;
-}
-
-//this function checks for a match
-var checkMatches = function (xaxis, yaxis) {
-xaxis = parseInt(xaxis);
-yaxis = parseInt(yaxis);
-console.log(dataStore);
-
-// the variables below store the amount of same color in a row in each direction for the most recently placed token
-  var left = 0;
-  var right = 0;
-  var down = 0;
-  var downleft = 0;
-  var downright = 0;
-  var upleft = 0;
-  var upright = 0;
-
-// these functions go in each direction of current token and calculate how many in a row of the same color there are
-
-  var leftCheck = function() {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (currentx-1 >= 0 && dataStore[currentx-1][yaxis] === currentColor) {
-      left++;
-      currentx--;
-    };
-  };
-  var rightCheck = function() {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (currentx+1 < 7 && dataStore[currentx+1][yaxis] === currentColor) {
-      right++;
-      currentx++;
-    };
-  };
-  var downCheck = function() {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (currenty-1 >= 0 && dataStore[xaxis][currenty-1] === currentColor) {
-      down++;
-      currenty--;
-    };
-  };
-  var downleftCheck = function() {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (currenty-1 >= 0 && currentx-1 >= 0 && dataStore[currentx-1][currenty-1] === currentColor) {
-      downleft++;
-      currenty--;
-      currentx--;
-    };
-  };
-  var downrightCheck = function() {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (currenty-1 >= 0 && currentx+1 < 7 && dataStore[currentx+1][currenty-1] === currentColor) {
-      downright++;
-      currenty--;
-      currentx++;
-    };
-  };
-  var upleftCheck = function() {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (currenty+1 < 6 && currentx-1 >= 0 && dataStore[currentx-1][currenty+1] === currentColor) {
-      upleft++;
-      currenty++;
-      currentx--;
-    };
-  };
-  var uprightCheck = function() {
-    var currentx = xaxis;
-    var currenty = yaxis;
-    while (currenty+1 < 6 && currentx+1 < 7 && dataStore[currentx+1][currenty+1] === currentColor) {
-      upright++;
-      currenty++;
-      currentx++;
-    };
-  };
-  leftCheck(); rightCheck();downCheck();downleftCheck();downrightCheck();upleftCheck();uprightCheck();
-  //
-  if (left+right >= 3 || down >= 3 || upleft + downright >= 3 || downleft + upright >= 3) {
-    $('[x='+xaxis+'][y='+yaxis+']').addClass('highlighted')
-    winner();
-  };
-  if (left+right >= 3) {
-    highlightTokens.left(xaxis,yaxis,left);
-    highlightTokens.right(xaxis,yaxis,right);
-  };
-  if (down >= 3) {
-    highlightTokens.down(xaxis,yaxis,down);
-  };
-  if (upleft + downright >= 3) {
-    highlightTokens.upleft(xaxis,yaxis,upleft);
-    highlightTokens.downright(xaxis,yaxis,downright);
-  };
-  if (downleft + upright >= 3) {
-    highlightTokens.downleft(xaxis,yaxis,downleft);
-    highlightTokens.upright(xaxis,yaxis,upright);
-  };
-};
-
-//takes coordinates and calls change color function and changes
-//data store values
-
-var colorSpace = function (xaxis, yaxis) {
-  var selectedSpace = $('[x='+xaxis+'][y='+yaxis+']')
-  selectedSpace.css('background-color', changeColor());
-  dataStore[xaxis][yaxis] = currentColor;
-  checkMatches(xaxis, yaxis);
-}
-
-//function that returns value of y value that should change color
-var columnHeight = function (column) {
-  colorSpace(column, yPositionAvailable[column]);
-  yPositionAvailable[column]++;
-  if (yPositionAvailable[column] === 6) {
-    $('#'+column).prop('disabled',true);
+  },
+  changeColor: function () {
+    if (game.currentColor === "red") {
+      game.currentColor = "black";
+    } else {
+      game.currentColor = "red"
+    }
+    UI.currentTurn();
   }
 }
 
-
-  //Action of dropping a token into a column
-  //decides what y-axis should change color
-  var droptoken = function () {
-    columnHeight(this.id);
-    movesMade++;
-    checkStaleMate();
-  }
-
-  //setting up column buttons
-  //setting up board spaces and column buttons
-  var buttonrow = $('#buttonrow')
-  var board = $('#board')
-
-  //this sets up the game board
-  var setup = function() {
+//this object stores methods that alter the UI
+var UI = {
+  currentTurn: function () {
+    if (game.currentColor === "red") {
+      $('#red').addClass('currentturn')
+      $('#black').removeClass('currentturn')
+    } else {
+      $('#red').removeClass('currentturn')
+      $('#black').addClass('currentturn')
+    }
+  },
+  highlightTokens: {
+    left: function(xaxis, yaxis, amount) {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (amount>0) {
+        currentx--;
+        $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
+        amount--;
+      };
+    },
+    right: function(xaxis, yaxis, amount) {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (amount>0) {
+        currentx++;
+        $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
+        amount--;
+      };
+    },
+    down: function(xaxis, yaxis, amount) {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (amount>0) {
+        currenty--;
+        $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
+        amount--;
+      };
+    },
+    downleft: function(xaxis, yaxis, amount) {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (amount>0) {
+        currenty--;
+        currentx--;
+        $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
+        amount--;
+      };
+    },
+    upleft: function(xaxis, yaxis, amount) {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (amount>0) {
+        currenty++;
+        currentx--;
+        $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
+        amount--;
+      };
+    },
+    downright: function(xaxis, yaxis, amount) {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (amount>0) {
+        currenty--;
+        currentx++;
+        $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
+        amount--;
+      };
+    },
+    upright: function(xaxis, yaxis, amount) {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (amount>0) {
+        currenty++;
+        currentx++;
+        $('[x='+currentx+'][y='+currenty+']').addClass('highlighted');
+        amount--;
+      };
+    }
+  },
+  reset: function () {
+    $('#center').html("");
+    $('#red').show();
+    $('#black').show();
+    $('#center').css('margin', 'inherit')
+    $('#buttonrow').show();
+    buttonrow.html("");
+    board.html("");
+    UI.setup();
+    game.dataStore = {
+      0: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
+      1: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
+      2: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
+      3: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
+      4: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
+      5: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""},
+      6: {0: "", 1: "", 2: "", 3: "", 4: "", 5: ""}
+    };
+    game.yPositionAvailable = {
+      0: 0,
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0
+    };
+    game.movesMade = 0;
+  },
+  staleMate: function() {
+    $('#center').html("STALE MATE!");
+    $('#red').hide();
+    $('#black').hide();
+    $('#center').css('margin', 'auto')
+    $('#center').css('margin-bottom', '46px')
+    $('#buttonrow').hide();
+  },
+  winner: function () {
+    $('#center').html(game.currentColor.toUpperCase() + " WINS!");
+    $('#red').hide();
+    $('#black').hide();
+    $('#center').css('margin', 'auto')
+    $('#center').css('margin-bottom', '46px')
+    $('#buttonrow').hide();
+  },
+  setup: function() {
     for (var x = 0; x < 7; x++) {
       for (var y = 0; y < 6; y++) {
         var space = $('<div>');
@@ -324,20 +170,143 @@ var columnHeight = function (column) {
       var button = $('<button>Drop Here</button>');
       button.attr('class', 'button');
       button.attr('id', x);
-      button.on('click', droptoken);
+      button.on('click', brain.droptoken);
       buttonrow.append(button);
     }
+    $('#reset').on('click', function() {
+    UI.reset();
+    });
   }
-  $('#reset').on('click', function() {
-    reset();
-  });
-  setup();
+}
+
+//this object stores methods that deal with logic matches/stalemate
+var logic = {
+  checkMatches: function (xaxis, yaxis) {
+    xaxis = parseInt(xaxis);
+    yaxis = parseInt(yaxis);
+    // the variables below store the amount of same color in a row in each direction for the most recently placed token
+    var left = 0;
+    var right = 0;
+    var down = 0;
+    var downleft = 0;
+    var downright = 0;
+    var upleft = 0;
+    var upright = 0;
+    // these functions go in each direction of current token and calculate how many in a row of the same color there are
+    var leftCheck = function() {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (currentx-1 >= 0 && game.dataStore[currentx-1][yaxis] === game.currentColor) {
+        left++;
+        currentx--;
+      };
+    };
+    var rightCheck = function() {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (currentx+1 < 7 && game.dataStore[currentx+1][yaxis] === game.currentColor) {
+        right++;
+        currentx++;
+      };
+    };
+    var downCheck = function() {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (currenty-1 >= 0 && game.dataStore[xaxis][currenty-1] === game.currentColor) {
+        down++;
+        currenty--;
+      };
+    };
+    var downleftCheck = function() {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (currenty-1 >= 0 && currentx-1 >= 0 && game.dataStore[currentx-1][currenty-1] === game.currentColor) {
+        downleft++;
+        currenty--;
+        currentx--;
+      };
+    };
+    var downrightCheck = function() {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (currenty-1 >= 0 && currentx+1 < 7 && game.dataStore[currentx+1][currenty-1] === game.currentColor) {
+        downright++;
+        currenty--;
+        currentx++;
+      };
+    };
+    var upleftCheck = function() {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (currenty+1 < 6 && currentx-1 >= 0 && game.dataStore[currentx-1][currenty+1] === game.currentColor) {
+        upleft++;
+        currenty++;
+        currentx--;
+      };
+    };
+    var uprightCheck = function() {
+      var currentx = xaxis;
+      var currenty = yaxis;
+      while (currenty+1 < 6 && currentx+1 < 7 && game.dataStore[currentx+1][currenty+1] === game.currentColor) {
+        upright++;
+        currenty++;
+        currentx++;
+      };
+    };
+    leftCheck(); rightCheck();downCheck();downleftCheck();downrightCheck();upleftCheck();uprightCheck();
+    //
+    if (left+right >= 3 || down >= 3 || upleft + downright >= 3 || downleft + upright >= 3) {
+      $('[x='+xaxis+'][y='+yaxis+']').addClass('highlighted')
+      UI.winner();
+    };
+    if (left+right >= 3) {
+      UI.highlightTokens.left(xaxis,yaxis,left);
+      UI.highlightTokens.right(xaxis,yaxis,right);
+    };
+    if (down >= 3) {
+      UI.highlightTokens.down(xaxis,yaxis,down);
+    };
+    if (upleft + downright >= 3) {
+      UI.highlightTokens.upleft(xaxis,yaxis,upleft);
+      UI.highlightTokens.downright(xaxis,yaxis,downright);
+    };
+    if (downleft + upright >= 3) {
+      UI.highlightTokens.downleft(xaxis,yaxis,downleft);
+      UI.highlightTokens.upright(xaxis,yaxis,upright);
+    };
+  },
+  checkStaleMate: function() {
+    if (game.movesMade === 42) {
+      UI.staleMate();
+    }
+  },
+  columnHeight: function (column) {
+    brain.colorSpace(column, game.yPositionAvailable[column]);
+    game.yPositionAvailable[column]++;
+    if (game.yPositionAvailable[column] === 6) {
+      $('#'+column).prop('disabled',true);
+    }
+  }
+}
+
+//this dictates what functions are called and when
+var brain = {
+  colorSpace: function (xaxis, yaxis) {
+    var selectedSpace = $('[x='+xaxis+'][y='+yaxis+']')
+    selectedSpace.css('background-color', game.currentColor);
+    game.dataStore[xaxis][yaxis] = game.currentColor;
+    logic.checkMatches(xaxis, yaxis);
+    game.changeColor()
+  },
+  droptoken: function () {
+  logic.columnHeight(this.id);
+  game.movesMade++;
+  logic.checkStaleMate();
+}
+}
+
+var buttonrow = $('#buttonrow')
+var board = $('#board')
+UI.setup();
+
 });
-
-
-//Status of the coordinates of colored tokens
-
-
-//Updating the UI of the board
-
-//check if win
